037b2b8440bf45cc76e2f3c870c714ee60152886
Ooi Kim Beng <gfish213@yahoo.com>
2024-02-20 15:24:34 +0800

    change again

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Hi, welcome to the world of Jenkins.");
+        System.out.println("Hi, welcome to the world of Jenkins.......");
     }
 
     public String getMessage() {
